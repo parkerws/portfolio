8163f1de0ba80ae8598f50dd62fd20ad2ffd5368
William Parker <47265263+parkerws@users.noreply.github.com>
2020-10-19 17:29:33 -0400

    Created about page

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(() => { 
-
+    $("#resume").click((e) => {
+        e.preventDefault();
+        window.location.href = 'files/William_Parker_Resume_10OCT2020.pdf'
+    })
 
     
-})
+});
