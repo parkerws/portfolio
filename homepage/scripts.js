5d114bfe28456b7b8a48dd5f10c102947fc1fd19
William Parker <47265263+parkerws@users.noreply.github.com>
2020-10-19 21:46:59 -0400

    Created contact page, added jquery form validation, hooked up formspree email

--- homepage/scripts.js
+++ homepage/scripts.js
@@ -3,6 +3,34 @@ $(document).ready(() => {
         e.preventDefault();
         window.location.href = 'files/William_Parker_Resume_10OCT2020.pdf'
     })
+    $(function() {
+        $("form[name='contact']").validate({
 
+            rules: {
+                _replyto: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 10
+                }
+            },
+
+            messages: {
+                _replyto: {
+                    required: "Please enter an email address",
+                    email: "Please enter a valid email address"
+                },
+                message: {
+                    required: "Please enter a message",
+                    minlength: "Please enter 10 or more characters"
+                }
+            },
+            submitHandler: function(form) {
+                form.submit();
+            }
+        })
+    })
     
 });
